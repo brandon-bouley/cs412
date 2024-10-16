62c85b3396968e7ce5074fdfa7c02768e77724cb
Brandon Bouley <bbouley@bu.edu>
2024-10-16 19:28:26 -0400

    Assignment 6

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=30)
@@ -9,3 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    def get_status_messages(self):
+        return self.statusmessage_set.all().order_by('-timestamp')
+
+    def get_absolute_url(self):
+        return reverse('show_profile', kwargs={'pk': self.pk})
+
+class StatusMessage(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Status by {self.profile.first_name} at {self.timestamp}"
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,11 +1,41 @@
+# mini_fb/views.py
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import ListView
-from .models import Profile
+from django.views.generic import DetailView, ListView, CreateView
+from .models import Profile, StatusMessage
+from .forms import CreateProfileForm, CreateStatusMessageForm
+from django.urls import reverse
 
 class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/show_profile.html'
+    context_object_name = 'profile'
+
+class CreateProfileView(CreateView):
+    model = Profile
+    form_class = CreateProfileForm
+    template_name = 'mini_fb/create_profile_form.html'
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.object.pk})
+
+class CreateStatusMessageView(CreateView):
+    model = StatusMessage
+    form_class = CreateStatusMessageForm
+    template_name = 'mini_fb/create_status_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
+        return context
+
+    def form_valid(self, form):
+        form.instance.profile = Profile.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
