89fe91f27c141a0fe94bde4173de20654d8500e2
Brandon Bouley <bbouley@bu.edu>
2024-10-23 18:58:09 -0400

    Assignment 7

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,8 +1,8 @@
 # mini_fb/views.py
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, CreateView
-from .models import Profile, StatusMessage
-from .forms import CreateProfileForm, CreateStatusMessageForm
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from .models import Profile, StatusMessage, Image
+from .forms import CreateProfileForm, CreateStatusMessageForm, UpdateProfileForm, UpdateStatusMessageForm
 from django.urls import reverse
 
 class ShowAllProfilesView(ListView):
@@ -34,8 +34,64 @@ class CreateStatusMessageView(CreateView):
         return context
 
     def form_valid(self, form):
-        form.instance.profile = Profile.objects.get(pk=self.kwargs['pk'])
+        # Save the status message to the database
+        sm = form.save(commit=False)
+        sm.profile = Profile.objects.get(pk=self.kwargs['pk'])
+        sm.save()
+
+        # Read the files from the form
+        files = self.request.FILES.getlist('files')
+        for file in files:
+            # Create an Image object for each file
+            image = Image(status_message=sm, image_file=file)
+            image.save()
+
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
+
+class UpdateProfileView(UpdateView):
+    model = Profile
+    form_class = UpdateProfileForm
+    template_name = 'mini_fb/update_profile_form.html'
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.object.pk})
+
+class DeleteStatusMessageView(DeleteView):
+    model = StatusMessage
+    template_name = 'mini_fb/delete_status_form.html'
+    context_object_name = 'status_message'
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
+
+class UpdateStatusMessageView(UpdateView):
+    model = StatusMessage
+    form_class = UpdateStatusMessageForm
+    template_name = 'mini_fb/update_status_form.html'
+
+    def form_valid(self, form):
+        # Save the status message to the database
+        sm = form.save()
+
+        # Read the files from the form
+        files = self.request.FILES.getlist('files')
+        for file in files:
+            # Create an Image object for each file
+            image = Image(status_message=sm, image_file=file)
+            image.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
+
+class DeleteImageView(DeleteView):
+    model = Image
+    template_name = 'mini_fb/delete_image_form.html'
+    context_object_name = 'image'
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.object.status_message.profile.pk})
